43d68f337665ff80989fbba6515e98930bd4a24a
slobben <daantjesl@gmail.com>
2025-06-12 13:37:20 +0200

    pushie wushie fixie wixie

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {GridViewComponent} from './features/grid-view.component';
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
+  imports: [GridViewComponent],
+})
+export class AppComponent {
+  protected title = 'Cell!';
+}
