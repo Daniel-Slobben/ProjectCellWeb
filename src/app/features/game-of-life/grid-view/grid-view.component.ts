954584590e5290d175de42db87e6a5a92041af5b
slobben <daantjesl@gmail.com>
2026-07-31 16:45:55 +0200

    Revert "removed unused caching logic in grid rendering logic"
    This reverts commit 7d60b9f08b881931ef4485d5d91651c4391afef9.

--- src/app/features/game-of-life/grid-view/grid-view.component.ts
+++ src/app/features/game-of-life/grid-view/grid-view.component.ts
@@ -28,6 +28,10 @@ export class GridViewComponent implements AfterViewInit, OnDestroy {
   protected cellOffsetY = 0;
   private renderer!: WebGLGridRenderer;
 
+  private drawnGeneration = 0;
+  private drawnCellOffsetX = 0;
+  private drawnCellOffsetY = 0;
+
   // Drag state
   private isDragging = false;
   private dragStartX = 0;
@@ -103,7 +107,15 @@ export class GridViewComponent implements AfterViewInit, OnDestroy {
   }
 
   private updateVisibleBlocks() {
+    if (this.drawnGeneration == this.blockService.getGeneration() &&
+      this.drawnCellOffsetX == this.cellOffsetX &&
+      this.drawnCellOffsetY == this.cellOffsetY) {
+      return;
+    }
     this.renderer.clear();
+    this.drawnGeneration = this.blockService.getGeneration();
+    this.drawnCellOffsetX = this.cellOffsetX;
+    this.drawnCellOffsetY = this.cellOffsetY;
 
     const startBlockX = Math.floor(this.cellOffsetX / this.blockSize);
     const startBlockY = Math.floor(this.cellOffsetY / this.blockSize);
